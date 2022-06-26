54c041f0e9ab43fca412eacba79d9421c17417d0
Vincent Richefeu <vincent.richefeu@3sr-grenoble.fr>
2022-06-26 11:07:52 +0200

    refactoring of linreg as a singleton

--- README.md
+++ README.md
@@ -7,7 +7,14 @@ These are **TO**ols **OF**ten **US**ed in my various simulation codes.
 
 Installation is simply a matter of copying the directory `toofus` somewhere on your computer.
 
-Since the path `/usr/local/include` should be known by the environnement, a header file can be included that way:
+A simple solution to get `toofus` in your home directory, is to copy-past the following command in a terminal (git needs to be installed)
+
+```
+cd ~ && rm -rf toofus && git clone https://github.com/richefeu/toofus.git && cd -
+```
+
+If `toofus` is installed in e.g. `/usr/local/include`,
+since this path should be known by the environment, a header file can simply be included that way:
 
 ```c++
 #include "toofus/vec3.hpp"
@@ -18,17 +25,12 @@ It is also possible to simply add the path of the folder at compilation:
 ```sh
 g++ ... -I /path/to/toofus ...
 ```
+For example: `g++ ... -I ~ ...` if the recommended installation has been chosen.
 
-In this case, the inclusion can be reduced to:
+In this case, the inclusion in a C++ source file can be reduced to:
 
 ```c++
 #include "vec3.hpp"
-```
-
-The recommended and easiest method of installation is to move to the downloaded `toofus` directory containing the header files and run the script `install.sh`:
-
-```sh
-sh install.sh
 ```
 
 
--- linreg.hpp
+++ linreg.hpp
@@ -18,50 +18,61 @@
 #include <iostream>
 #include <vector>
 
-namespace {
-
-/// @brief Make a linear regression of a set of (x, y) pairs
-/// @return Returns a, b so that the line ax+b is the linear regression of data x and y.
-///         r is the coefficient of correlation.
-void linreg(std::vector<double> &x, std::vector<double> &y, double &a, double &b, double &r) {
-  if (x.size() != y.size()) {
-    std::cerr << "@linreg, x and y must have the same size" << std::endl;
-    return;
-  }
-  int N = static_cast<int>(x.size());
-  if (N == 0)
-    return;
-  double invN = 1.0f / (double)N;
-
-  double xmean = 0.0;
-  double ymean = 0.0;
-  for (int i = 0; i < N; i++) {
-    xmean += x[i];
-    ymean += y[i];
-  }
-  xmean *= invN;
-  ymean *= invN;
-
-  double Sx = 0.0;
-  double Sy = 0.0;
-  double Sxy = 0.0;
-  for (int i = 0; i < N; i++) {
-    double dx = x[i] - xmean;
-    double dy = y[i] - ymean;
-    Sx += dx * dx;
-    Sy += dy * dy;
-    Sxy += dx * dy;
+class linreg {
+public:
+  static linreg *get() {
+    static linreg instance;
+    return &instance;
   }
 
-  a = Sxy / Sx; // suppose that Sx cannot be zero
-  b = ymean - a * xmean;
-  if (Sy == 0.0) {
-    r = 1.0;
-  } else {
-    r = Sxy / sqrt(Sx * Sy);
-  }
-}
+  double orig;
+  double slope;
+  double err;
 
-} // end namespace
+  /// @brief Make a linear regression of a set of (x, y) pairs
+  ///        it computes orig, slope and err so that the line orig x + +slope is the linear regression of data x and y.
+  ///        err is the coefficient of correlation.
+  void run(std::vector<double> &x, std::vector<double> &y) {
+    if (x.size() != y.size()) {
+      std::cerr << "@linreg, x and y must have the same size" << std::endl;
+      return;
+    }
+    int N = static_cast<int>(x.size());
+    if (N == 0)
+      return;
+    double invN = 1.0f / (double)N;
+
+    double xmean = 0.0;
+    double ymean = 0.0;
+    for (int i = 0; i < N; i++) {
+      xmean += x[i];
+      ymean += y[i];
+    }
+    xmean *= invN;
+    ymean *= invN;
+
+    double Sx = 0.0;
+    double Sy = 0.0;
+    double Sxy = 0.0;
+    for (int i = 0; i < N; i++) {
+      double dx = x[i] - xmean;
+      double dy = y[i] - ymean;
+      Sx += dx * dx;
+      Sy += dy * dy;
+      Sxy += dx * dy;
+    }
+
+    orig = Sxy / Sx; // suppose that Sx cannot be zero
+    slope = ymean - orig * xmean;
+    if (Sy == 0.0) {
+      err = 1.0;
+    } else {
+      err = Sxy / sqrt(Sx * Sy);
+    }
+  }
+
+private:
+  linreg() {}
+};
 
 #endif /* end of include guard: LINREG_HPP */
